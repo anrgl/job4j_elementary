9ef294a64775d086f31c0b354690d63a3501b2b0
aynur <galimovaynur@protonmail.com>
2020-09-12 23:44:59 +0500

    6.0.2. Заполнение массива. [#357207]

--- src/ru/job4j/ArrayDefinition.java
+++ src/ru/job4j/ArrayDefinition.java
@@ -9,5 +9,17 @@ public class ArrayDefinition {
         System.out.println("Ages size " + ages.length);
         System.out.println("Surnames size " + surnames.length);
         System.out.println("Prices size " + prices.length);
+
+        String[] names = new String[4];
+
+        names[0] = "Petr Arsentev";
+        names[1] = "Aynur Galimov";
+        names[2] = "Ivan Ivanov";
+        names[3] = "Svetlana Mironova";
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
