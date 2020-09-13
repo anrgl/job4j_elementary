ce8b23d8ab2d702dec92de53eb8fc16b432133ec
aynur <galimovaynur@protonmail.com>
2020-09-14 00:07:08 +0500

    6.5.2. Поиск минимума в диапазоне. [#357203]

---
+++ src/ru/job4j/MinDiapason.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+public class MinDiapason {
+    public static int findMin(int[] array, int start, int finish) {
+        int min = array[start];
+        for (; start <= finish; start++) {
+            if (min > array[start]) {
+                min = array[start];
+            }
+        }
+        return min;
+    }
+}
